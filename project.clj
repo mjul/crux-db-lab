1c8df97f7e0b2c2487ef62f20199e6bb3b9583e7
Martin Jul <no-spam@mjul.com>
2019-04-24 13:51:02 +0200

    Add Crux dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject crux-db-lab "0.1.0-SNAPSHOT"
   :description "Playing around with the Crux database"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [juxt/crux "19.04-1.0.3-alpha-SNAPSHOT"]]
   :repl-options {:init-ns crux-db-lab.core})
