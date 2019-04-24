b48fda22b9d63167889d550e5e0d5baae03dd9e8
Martin Jul <no-spam@mjul.com>
2019-04-24 14:02:03 +0200

    Set up in-memory Crux database engine

--- src/crux_db_lab/core.clj
+++ src/crux_db_lab/core.clj
@@ -1,4 +1,22 @@
-(ns crux-db-lab.core)
+(ns crux-db-lab.core
+  (:require [crux.api :as crux])
+  (:import [crux.api ICruxAPI]))
+
+
+(defn start-system
+  "Start an in-memory Crux databse server"
+  []
+  (crux/start-standalone-system {:kv-backend "crux.kv.memdb.MemKv"
+                                 :db-dir "data/db-dir-1"}))
+
+(defn stop-system
+  "Stop the Crux database server."
+  []
+  (.close system))
+
+(def ^crux.api.ICruxAPI system
+  (start-system))
+
 
 (defn foo
   "I don't do a whole lot."
