b62ad731bee06fd6ef591edf4d960cc4c11b1e80
Martin Jul <no-spam@mjul.com>
2019-04-24 13:33:06 +0200

    Initialize project from template

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject crux-db-lab "0.1.0-SNAPSHOT"
+  :description "Playing around with the Crux database"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns crux-db-lab.core})
---
+++ test/crux_db_lab/core_test.clj
@@ -0,0 +1,7 @@
+(ns crux-db-lab.core-test
+  (:require [clojure.test :refer :all]
+            [crux-db-lab.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    #_(is (= 0 1))))
